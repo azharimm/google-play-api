8a01f2f151b973cc01588b4740ce76e671f3a3a5
Azhari Marzan <azhari.marzan@gmail.com>
2020-12-13 09:52:49 +0700

    cors

--- src/app.js
+++ src/app.js
@@ -4,6 +4,13 @@ const middlewares = require("./utils/middlewares");
 const router = require('./routes/router');
 
 const app = express();
+//cors
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://azharimm.tk');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    next();
+});
 
 app.use('/', router);
 app.use(middlewares.notFound);
